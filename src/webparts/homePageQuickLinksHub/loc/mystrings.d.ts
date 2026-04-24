443e63406682130587d9034025ed0e4e7723d2da
Vivek Gohil <gohilvivek0303@gmail.com>
2026-04-24 19:14:40 +0530

    WorkWise Webpart Setup

---
+++ src/webparts/homePageQuickLinksHub/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IHomePageQuickLinksHubWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'HomePageQuickLinksHubWebPartStrings' {
+  const strings: IHomePageQuickLinksHubWebPartStrings;
+  export = strings;
+}
